5285ca3657357cb5df213520a17ce4fa43342e2b
abdou <abdououni67@gmail.com>
2025-05-15 17:42:26 +0100

    added event and two mmethods to trigger it

--- Test/DoSomthing.cs
+++ Test/DoSomthing.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Test
 {
-    internal class DoSomthing
+
+    public class DoSomthing
     {
+
+        public event Func<int, int>? Somthing = X => X + 1;
+     
+
+      
+
+        public void triger(Predicate<int> condition)
+        {
+
+            int x = 10;
+            if (condition(x))
+            {
+                OnSomethinghappend(x);
+            }
+
+        }
+
+        public void OnSomethinghappend(int x)
+        {
+            if (Somthing != null)
+            {
+                Somthing?.Invoke(x);
+            }
+        }
     }
 }
+    
+
